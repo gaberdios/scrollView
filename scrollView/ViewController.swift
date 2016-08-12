be50e95f488320244401ced1b6b99155ee8c4f28
gaberdios <gaberdios@gmail.com>
2016-08-12 22:31:50 +0200

    version 3.0 Time 20:30

--- scrollView/ViewController.swift
+++ scrollView/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var contentWidth: CGFloat = 0.0
+        
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
             let imageView = UIImageView(image: image)
@@ -25,6 +27,8 @@ class ViewController: UIViewController {
             var newX: CGFloat = 0.0
             newX = view.frame.midX + view.frame.size.width * CGFloat(x)
             
+            contentWidth += newX
+            
             scrollView.addSubview(imageView)
             
             imageView.frame = CGRect(x: newX - 75, y: view.frame.size.height / 2 - 75, width: 150, height: 150)
@@ -32,7 +36,7 @@ class ViewController: UIViewController {
             
         }
         
-        print("Count: \(images.count)")
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
         
     }
 }
